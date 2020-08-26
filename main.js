f56215ac4d659db70d82e0cac545a905f0551325
Ghidyon <gideonakunana@gmail.com>
2020-08-26 05:32:25 +0100

    modified conditions

--- main.js
+++ main.js
@@ -5,23 +5,40 @@ function validate() {
     let contact = document.formGet.contactNum.value;
     let email = document.formGet.email.value;
     let dob = document.formGet.dateOfBirth.value;
-    let nationality = document.formGet.country.value;
     let origin = document.formGet.origin.value;
 
     //Define variables for date conditions
-    let realDate = new Date();
-    let commencedDate = new Date(2020, 7, 29);
+    let currentDate = new Date();
+    let commence = new Date(2020, 7, 29);
     let deadline = new Date(2020, 7, 31, 11, 59, 59);
 
     //Define conditions for any empty field
-    if ((fName, lName, contact, email, dob, nationality, origin == null) || (fName, lName, contact, email, dob, nationality, origin == "")) {
-        alert('Fill empty fields');
-        return false;
+    if (fName == "" && lName == "" && contact == "" && email == "" && dob == "" && origin == "") {
+        alert('Please fill empty fields.')
+        return false; //checks if all input fields are empty
+    } else if (fName == null || fName == "" || fName == " ") {
+        alert('Please fill first name field.');
+        return false; //checks if first name field is empty
+    } else if (lName == null || lName == "" || lName == " ") {
+        alert('Please fill last name field.');
+        return false; //checks if last name field is empty
+    } else if (contact == null || contact == "" || contact == " ") {
+        alert('Please fill contact field.');
+        return false; //checks if contact name field is empty
+    } else if (email == null || email == "" || email == " ") {
+        alert('Please fill email field.');
+        return false; //checks if email field is empty
+    } else if (dob == null || dob == "" || dob == " ") {
+        alert('Please fill date of birth field.');
+        return false; //checks if date of birth field is empty
+    } else if (origin == null || origin == "" || origin == " ") {
+        alert('Please fill state of origin field.');
+        return false;  //checks if state of origin field is empty
     }
 
     //Define conditions for registration period
-    if (realDate >= commencedDate && realDate <= deadline) {
-        alert('Registration successful');
+    if (currentDate >= commence && currentDate <= deadline) {
+        alert('Registration successful.');
         return true;
     } else {
         alert('Registration period has ended');
